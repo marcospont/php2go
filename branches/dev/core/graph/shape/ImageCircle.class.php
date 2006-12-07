ab2283dddd5d9f1f4d1f8154f77cd7879bb8fec9
marcos.pont <marcos.pont@713797e6-a6b2-11dd-9cdb-aff5a21a0eb7>
2006-12-07 20:14:30 +0000

    Translation of source comments to english and phpdoc format

--- branches/dev/core/graph/shape/ImageCircle.class.php
+++ branches/dev/core/graph/shape/ImageCircle.class.php
@@ -1,59 +1,69 @@
 <?php
-//
-// +----------------------------------------------------------------------+
-// | PHP2Go Web Development Framework                                     |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2002-2006 Marcos Pont                                  |
-// +----------------------------------------------------------------------+
-// | This library is free software; you can redistribute it and/or        |
-// | modify it under the terms of the GNU Lesser General Public           |
-// | License as published by the Free Software Foundation; either         |
-// | version 2.1 of the License, or (at your option) any later version.   |
-// | 																	  |
-// | This library is distributed in the hope that it will be useful,      |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    |
-// | Lesser General Public License for more details.                      |
-// | 																	  |
-// | You should have received a copy of the GNU Lesser General Public     |
-// | License along with this library; if not, write to the Free Software  |
-// | Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA             |
-// | 02111-1307  USA                                                      |
-// +----------------------------------------------------------------------+
-//
-// $Header: /www/cvsroot/php2go/core/graph/shape/ImageCircle.class.php,v 1.3 2006/02/28 21:55:57 mpont Exp $
-// $Date: 2006/02/28 21:55:57 $
+/**
+ * PHP2Go Web Development Framework
+ *
+ * Copyright (c) 2002-2006 Marcos Pont
+ *
+ * LICENSE:
+ *
+ * This library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any
+ * later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @author Marcos Pont <mpont@users.sourceforge.net>
+ * @copyright 2002-2006 Marcos Pont
+ * @license http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @version $Id$
+ */
 
-//-----------------------------------------
 import('php2go.graph.shape.ImageArc');
-//-----------------------------------------
 
-//!-----------------------------------------------------------------
-// @class		ImageCircle
-// @desc		Círculo, com coordenadas X e Y do centro e tamanho do raio
-// @package		php2go.graph.shape
-// @extends		ImageArc
-// @author		Marcos Pont
-// @version		$Revision: 1.3 $
-//!-----------------------------------------------------------------
+/**
+ * Draws a circle
+ *
+ * A circle is represented by a center point
+ * and a radius.
+ *
+ * @package graph
+ * @subpackage shape
+ * @author Marcos Pont <mpont@users.sourceforge.net>
+ * @version $Revision$
+ */
 class ImageCircle extends ImageArc
 {
-	var $radius;	// @var radius int	Raio do círculo
-	
-	//!-----------------------------------------------------------------
-	// @function	ImageCircle::ImageCircle
-	// @desc		Construtor da classe
-	// @access		public
-	// @param		cx int				Coordenada X do centro
-	// @param		cy int				Coordenada Y do centro
-	// @param		radiu int			Raio do círculo
-	// @param		fill bool			"FALSE" Possui ou não preenchimento
-	// @param		shadow int			"0" Tamanho da sombra do círculo
-	// @param		shadowColor mixed	"NULL" Cor da sombra
-	//!-----------------------------------------------------------------
+	/**
+	 * Circle radius
+	 *
+	 * @var float
+	 * @access private
+	 */
+	var $radius;
+
+	/**
+	 * Class constructor
+	 *
+	 * @param int $cx Center X
+	 * @param int $cy Center Y
+	 * @param float $radius Radius
+	 * @param mixed $fill Fill style
+	 * @param int $shadow Shadow size
+	 * @param string|array $shadowColor Shadow color
+	 * @return ImageCircle
+	 */
 	function ImageCircle($cx, $cy, $radius, $fill=FALSE, $shadow=0, $shadowColor=NULL) {
 		parent::ImageArc($cx, $cy, $radius*2, $radius*2, 0, 360, $fill, $shadow, $shadowColor);
 		$this->radius = $radius;
-	}	
+	}
 }
 ?>
--- branches/dev/core/graph/shape/ImageRectangle.class.php
+++ branches/dev/core/graph/shape/ImageRectangle.class.php
@@ -1,55 +1,95 @@
 <?php
-//
-// +----------------------------------------------------------------------+
-// | PHP2Go Web Development Framework                                     |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2002-2006 Marcos Pont                                  |
-// +----------------------------------------------------------------------+
-// | This library is free software; you can redistribute it and/or        |
-// | modify it under the terms of the GNU Lesser General Public           |
-// | License as published by the Free Software Foundation; either         |
-// | version 2.1 of the License, or (at your option) any later version.   |
-// | 																	  |
-// | This library is distributed in the hope that it will be useful,      |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    |
-// | Lesser General Public License for more details.                      |
-// | 																	  |
-// | You should have received a copy of the GNU Lesser General Public     |
-// | License along with this library; if not, write to the Free Software  |
-// | Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA             |
-// | 02111-1307  USA                                                      |
-// +----------------------------------------------------------------------+
-//
-// $Header: /www/cvsroot/php2go/core/graph/shape/ImageRectangle.class.php,v 1.3 2006/02/28 21:55:57 mpont Exp $
-// $Date: 2006/02/28 21:55:57 $
+/**
+ * PHP2Go Web Development Framework
+ *
+ * Copyright (c) 2002-2006 Marcos Pont
+ *
+ * LICENSE:
+ *
+ * This library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any
+ * later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @author Marcos Pont <mpont@users.sourceforge.net>
+ * @copyright 2002-2006 Marcos Pont
+ * @license http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @version $Id$
+ */
 
-//!-----------------------------------------------------------------
-// @class		ImageRectangle
-// @desc		Representa um retângulo com duas coordenadas X,Y, com ou sem preenchimento
-// @package		php2go.graph.shape
-// @extends		Drawable
-// @author		Marcos Pont
-// @version		$Revision: 1.3 $
-//!-----------------------------------------------------------------
+/**
+ * Draws a rectangle
+ *
+ * A rectangle is represented by a pair of points: left upper
+ * and bottom right.
+ *
+ * @package graph
+ * @subpackage shape
+ * @author Marcos Pont <mpont@users.sourceforge.net>
+ * @version $Revision$
+ */
 class ImageRectangle extends Drawable
 {
-	var $x1;			// @var x1 int		Coordenada X inicial
-	var $y1;			// @var y1 int		Coordenada Y inicial
-	var $x2;			// @var x2 int		Coordenada X final
-	var $y2;			// @var y2 int		Coordenada Y final
-	var $fill = FALSE;	// @var fill bool	"FALSE" Indica se o retângulo deve ser preenchido
-	
-	//!-----------------------------------------------------------------
-	// @function	ImageRectangle::ImageRectangle
-	// @desc		Construtor do objeto
-	// @access		public
-	// @param		x1 int		Coordenada X inicial
-	// @param		y1 int		Coordenada Y inicial
-	// @param		x2 int		Coordenada X final
-	// @param		y2 int		Coordenada Y final
-	// @param		fill bool	"FALSE" Preencher ou não o retângulo ao desenhar
-	//!-----------------------------------------------------------------
+	/**
+	 * Left X coordinate
+	 *
+	 * @var int
+	 * @access private
+	 */
+	var $x1;
+
+	/**
+	 * Upper Y coordinate
+	 *
+	 * @var int
+	 * @access private
+	 */
+	var $y1;
+
+	/**
+	 * Right X coordinate
+	 *
+	 * @var int
+	 * @access private
+	 */
+	var $x2;
+
+	/**
+	 * Bottom Y coordinate
+	 *
+	 * @var int
+	 * @access private
+	 */
+	var $y2;
+
+	/**
+	 * Fill style
+	 *
+	 * @var mixed
+	 * @access private
+	 */
+	var $fill = FALSE;
+
+	/**
+	 * Class constructor
+	 *
+	 * @param int $x1 Left X
+	 * @param int $y1 Upper Y
+	 * @param int $x2 Right X
+	 * @param int $y2 Bottom Y
+	 * @param mixed $fill Fill style
+	 * @return ImageRectangle
+	 */
 	function ImageRectangle($x1, $y1, $x2, $y2, $fill=FALSE) {
 		parent::Drawable();
 		$this->x1 = $x1;
@@ -58,16 +98,14 @@ class ImageRectangle extends Drawable
 		$this->y2 = $y2;
 		$this->fill = (bool)$fill;
 	}
-	
-	//!-----------------------------------------------------------------
-	// @function	ImageRectangle::draw
-	// @desc		Desenha o retângulo na imagem $Img
-	// @access		public
-	// @param		&Img Image object	Imagem onde o retângulo deve ser desenhado
-	// @return		void
-	//!-----------------------------------------------------------------
+
+	/**
+	 * Draws the rectangle in a given image
+	 *
+	 * @param Image &$Img Target image
+	 */
 	function draw(&$Img) {
-		if ($this->fill)		
+		if ($this->fill)
 			imagefilledrectangle($Img->handle, $this->x1, $this->y1, $this->x2, $this->y2, $Img->currentColor);
 		else
 			imagerectangle($Img->handle, $this->x1, $this->y1, $this->x2, $this->y2, $Img->currentColor);
