4fa78247ce4ff984686e97ef7a48a9fe010881d5
marcos.pont <marcos.pont@713797e6-a6b2-11dd-9cdb-aff5a21a0eb7>
2006-12-21 17:26:05 +0000

    Translation of source comments to english and phpdoc format Removed delete() method

--- branches/dev/core/session/SessionManager.class.php
+++ branches/dev/core/session/SessionManager.class.php
@@ -1,57 +1,47 @@
 <?php
-//
-// +----------------------------------------------------------------------+
-// | PHP2Go Web Development Framework                                     |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2002-2006 Marcos Pont                                  |
-// +----------------------------------------------------------------------+
-// | This library is free software; you can redistribute it and/or        |
-// | modify it under the terms of the GNU Lesser General Public           |
-// | License as published by the Free Software Foundation; either         |
-// | version 2.1 of the License, or (at your option) any later version.   |
-// | 																	  |
-// | This library is distributed in the hope that it will be useful,      |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    |
-// | Lesser General Public License for more details.                      |
-// | 																	  |
-// | You should have received a copy of the GNU Lesser General Public     |
-// | License along with this library; if not, write to the Free Software  |
-// | Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA             |
-// | 02111-1307  USA                                                      |
-// +----------------------------------------------------------------------+
-//
-// $Header: /www/cvsroot/php2go/core/session/SessionManager.class.php,v 1.19 2006/11/25 11:58:50 mpont Exp $
-// $Date: 2006/11/25 11:58:50 $
+/**
+ * PHP2Go Web Development Framework
+ *
+ * Copyright (c) 2002-2006 Marcos Pont
+ *
+ * LICENSE:
+ *
+ * This library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any
+ * later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @author Marcos Pont <mpont@users.sourceforge.net>
+ * @copyright 2002-2006 Marcos Pont
+ * @license http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @version $Id$
+ */
 
-//!-----------------------------------------------------------------
-// @class 		SessionManager
-// @desc 		Esta classe é responsável por manipular variáveis simples
-// 				de sessão, que preferencialmente possuam valores escalares
-// 				ou do tipo array. Gerencia variáveis de sessão permitindo
-// 				criá-las, atribuir e recuperar valores
-// @package		php2go.session
-// @extends 	PHP2Go
-// @author 		Marcos Pont
-// @version		$Revision: 1.19 $
-//!-----------------------------------------------------------------
+/**
+ * Manages session variables
+ *
+ * @package session
+ * @author Marcos Pont <mpont@users.sourceforge.net>
+ * @version $Revision$
+ */
 class SessionManager extends PHP2Go
 {
-	//!-----------------------------------------------------------------
-	// @function	SessionManager::SessionManager
-	// @desc		Construtor da classe
-	// @access 		public
-	//!-----------------------------------------------------------------
-	function SessionManager() {
-		parent::PHP2Go();
-	}
-
-	//!-----------------------------------------------------------------
-	// @function	SessionManager::&getInstance
-	// @desc		Retorna una instância única (singleton) da classe SessionManager
-	// @return		SessionManager object
-	// @access		public	
-	//!-----------------------------------------------------------------
+	/**
+	 * Get the singleton of the SessionManager class
+	 *
+	 * @return SessionManager
+	 * @static
+	 */
 	function &getInstance() {
 		static $instance;
 		if (!isset($instance))
@@ -59,59 +49,18 @@ class SessionManager extends PHP2Go
 		return $instance;
 	}
 
-	//!-----------------------------------------------------------------
-	// @function	SessionManager::getSessionId
-	// @desc		Busca o ID da sessão atual
-	// @return		string ID da sessão
-	// @access		public	
-	//!-----------------------------------------------------------------
-	function getSessionId() {
-		return @session_id();
-	}
-
-	//!-----------------------------------------------------------------
-	// @function	SessionManager::getSessionName
-	// @desc		Obtém o nome da sessão atual
-	// @return		string Nome da sessão
-	// @access		public	
-	//!-----------------------------------------------------------------
-	function getSessionName() {
-		return @session_name();
-	}
-
-	//!-----------------------------------------------------------------
-	// @function	SessionManager::getSessionSavePath
-	// @desc		Obtém o caminho onde os dados da sessão são gravados no servidor
-	// @return		string Caminho de armazenamento da sessão
-	// @access		public	
-	//!-----------------------------------------------------------------
-	function getSessionSavePath() {
-		return @session_save_path();
-	}
-
-	//!-----------------------------------------------------------------
-	// @function	SessionManager::getValue
-	// @desc 		Busca o valor armazenado para uma variável de sessão
-	// @param		name string	Nome da variável solicitada
-	// @return 		mixed Valor da variável de sessão ou NULL caso ela não
-	// 				possua valor setado ou armazenado
-	// @access 		public	
-	//!-----------------------------------------------------------------
-	function getValue($name) {
-		if ($this->isRegistered($name))
-			return $_SESSION[$name];
-		return FALSE;
-	}
-
-	//!-----------------------------------------------------------------
-	// @function	SessionManager::getObjectProperty
-	// @desc		Método estático para a busca de valores de propriedades
-	//				armazenados em objetos de sessão
-	// @param		qualifiedName string	String contendo nome do objeto de sessão e nome da propriedade. Ex: sessao:variavel
-	// @return		mixed Valor da propriedade, se existente, ou NULL
-	// @access		public	
-	// @static
-	//!-----------------------------------------------------------------
+	/**
+	 * Read a property of a {@link SessionObject} instance
+	 * stored in the session scope
+	 *
+	 * The $qualifiedName variable must be composed by the
+	 * name of the session object plus the name of the property,
+	 * separated by a double colon sign.
+	 *
+	 * @param string $qualifiedName Qualified name
+	 * @return mixed
+	 * @static
+	 */
 	function getObjectProperty($qualifiedName) {
 		if (ereg("([^\:]+)\:(.+)", $qualifiedName, $matches)) {
 			import('php2go.session.SessionObject');
@@ -122,84 +71,98 @@ class SessionManager extends PHP2Go
 		return NULL;
 	}
 
-	//!-----------------------------------------------------------------
-	// @function	SessionManager::setValue
-	// @desc 		Atribui um valor a uma variável de sessão
-	// @param		name string	Nome da variável
-	// @param 		value mixed	Valor a ser atribuído à variável
-	// @return 		bool Retorna TRUE se o valor for setado ou FALSE se seu tipo
-	// 				não for array/escalar ou se o método for executado
-	// 				a partir da classe extendida SessionObject
-	// @note		Para atribuir objetos ao valor de uma sessão, utilize
-	//				a classe SessionObject
-	// @access 		public
-	//!-----------------------------------------------------------------
-	function setValue($name, $value) {
-		if (!$this->isA('SessionManager') || (!is_scalar($value) && !is_array($value)))
-			return FALSE;
-		$_SESSION[$name] = $value;
-		return TRUE;
+	/**
+	 * Get current session ID
+	 *
+	 * @return string
+	 */
+	function getSessionId() {
+		return @session_id();
 	}
 
-	//!-----------------------------------------------------------------
-	// @function 	SessionManager::isRegistered
-	// @desc 		Verifica se uma determinada variável está registrada na sessão
-	// @param		var string	Nome da variável
-	// @access 		public
-	// @return		bool
-	//!-----------------------------------------------------------------
+	/**
+	 * Get current session cookie name
+	 *
+	 * @return string
+	 */
+	function getSessionName() {
+		return @session_name();
+	}
+
+	/**
+	 * Get current session save path
+	 *
+	 * @return string
+	 */
+	function getSessionSavePath() {
+		return @session_save_path();
+	}
+
+	/**
+	 * Check if a given variable exists in the session scope
+	 *
+	 * @param string $var Variable name
+	 * @return bool
+	 */
 	function isRegistered($var) {
 		return (array_key_exists($var, $_SESSION));
 	}
 
-	//!-----------------------------------------------------------------
-	// @function 	SessionManager::register
-	// @desc 		Registra uma variável na sessão atual com um determinado valor
-	// @note		Para armazenar objetos na sessão, utilize a classe SessionObject
-	// @param		name string	Nome da variável
-	// @param		value mixed	Valor para a variável
-	// @access 		public
-	// @return		bool
-	//!-----------------------------------------------------------------
-	function register($name, $value) {
-		$_SESSION[$name] = $value;
-		return TRUE;
+	/**
+	 * Get the value of a session variable
+	 *
+	 * @param string $name Variable name
+	 * @return mixed
+	 */
+	function getValue($name) {
+		if ($this->isRegistered($name))
+			return $_SESSION[$name];
+		return FALSE;
 	}
 
-	//!-----------------------------------------------------------------
-	// @function 	SessionManager::unregister
-	// @desc 		Apaga uma variável da sessão atual
-	// @param		name string	Nome da variável de sessão
-	// @return 		bool Retorna TRUE se a variável estava registrada (sucesso) ou
-	// 				FALSE se ela não estava (falha)
-	// @access 		public
-	//!-----------------------------------------------------------------
+	/**
+	 * Create or replace a session variable
+	 *
+	 * @param string $name Variable name
+	 * @param mixed $value Variable value
+	 * @return Old value, if existent
+	 */
+	function setValue($name, $value) {
+		if (array_key_exists($name, $_SESSION))
+			$old = $_SESSION[$name];
+		else
+			$old = NULL;
+		$_SESSION[$name] = $value;
+		return $old;
+	}
+
+	/**
+	 * Create or replace a session variable
+	 *
+	 * @param string $name Variable name
+	 * @param mixed $value Variable value
+	 */
+	function register($name, $value) {
+		$_SESSION[$name] = $value;
+	}
+
+	/**
+	 * Removes a variable from the session scope
+	 *
+	 * @param string $name Variable name
+	 */
 	function unregister($name) {
-		if ($this->isRegistered($name)) {
+		if (array_key_exists($name, $_SESSION)) {
 			unset($_SESSION[$name]);
 			return TRUE;
 		}
 		return FALSE;
 	}
 
-	//!-----------------------------------------------------------------
-	// @function	SessionManager::delete
-	// @desc		Este método é um alias para SessionManager::unregister
-	// @param		name string	Nome da variável de sessão
-	// @access		public
-	// @return		bool
-	//!-----------------------------------------------------------------
-	function delete($name) {
-		$this->unregister($name);
-	}
-
-	//!-----------------------------------------------------------------
-	// @function	SessionManager::destroy
-	// @desc		Destrói todas as variáveis de sessão registradas
-	// @access		public
-	// @return		void
-	//!-----------------------------------------------------------------
-	function destroy() {		
+	/**
+	 * Destroys all session variables and kills the current active session
+	 */
+	function destroy() {
 		if (isset($_COOKIE[session_name()]))
 			setcookie(session_name(), '', time()-86400, '/');
 		session_unset();
