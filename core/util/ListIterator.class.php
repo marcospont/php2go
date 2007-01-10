8cc478f1191ffa5b60bdcdb8fab633601054ee63
marcos.pont <marcos.pont@713797e6-a6b2-11dd-9cdb-aff5a21a0eb7>
2007-01-10 20:41:40 +0000

    Translation of source comments to english and phpdoc format

--- core/util/ListIterator.class.php
+++ core/util/ListIterator.class.php
@@ -1,53 +1,72 @@
 <?php
-//
-// +----------------------------------------------------------------------+
-// | PHP2Go Web Development Framework                                     |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2002-2006 Marcos Pont                                  |
-// +----------------------------------------------------------------------+
-// | This library is free software; you can redistribute it and/or        |
-// | modify it under the terms of the GNU Lesser General Public           |
-// | License as published by the Free Software Foundation; either         |
-// | version 2.1 of the License, or (at your option) any later version.   |
-// | 																	  |
-// | This library is distributed in the hope that it will be useful,      |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    |
-// | Lesser General Public License for more details.                      |
-// | 																	  |
-// | You should have received a copy of the GNU Lesser General Public     |
-// | License along with this library; if not, write to the Free Software  |
-// | Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA             |
-// | 02111-1307  USA                                                      |
-// +----------------------------------------------------------------------+
-//
-// $Header: /www/cvsroot/php2go/core/util/ListIterator.class.php,v 1.10 2006/05/07 15:23:54 mpont Exp $
-// $Date: 2006/05/07 15:23:54 $
+/**
+ * PHP2Go Web Development Framework
+ *
+ * Copyright (c) 2002-2006 Marcos Pont
+ *
+ * LICENSE:
+ *
+ * This library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any
+ * later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @author Marcos Pont <mpont@users.sourceforge.net>
+ * @copyright 2002-2006 Marcos Pont
+ * @license http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @version $Id$
+ */
 
-//!-----------------------------------------------------------------
-// @class		ListIterator
-// @desc		Implementa iteração em listas permitindo a captura dos
-//				elementos da mesma em ambas as direções e obter a posição
-//				atual do cursor
-// @package		php2go.util
-// @extends		PHP2Go
-// @author		Marcos Pont
-// @version		$Revision: 1.10 $
-//!-----------------------------------------------------------------
+/**
+ * Iterator implementation
+ *
+ * The ListIterator class expects an instance of the AbstractList in the
+ * constructor. Based on it, the object can iterate through the list members
+ * forward and backwards, and jump to a specific list index.
+ *
+ * @package util
+ * @uses AbstractList
+ * @uses TypeUtils
+ * @author Marcos Pont <mpont@users.sourceforge.net>
+ * @version $Revision$
+ */
 class ListIterator extends PHP2Go
 {
-	var $current;	// @var current int					Posição atual do cursor
-	var $_List;		// @var _List AbstractList object	Lista abstrata da qual esta classe retorna os elementos
+	/**
+	 * Current cursor position
+	 *
+	 * @var int
+	 * @access private
+	 */
+	var $current;
 
-	//!-----------------------------------------------------------------
-	// @function	ListIterator::ListIterator
-	// @desc		Construtor da classe
-	// @param		List AbstractList object Instância da classe AbstractList
-	// @access		public	
-	//!-----------------------------------------------------------------
+	/**
+	 * List instance
+	 *
+	 * @var object AbstractList
+	 * @access private
+	 */
+	var $_List;
+
+	/**
+	 * Class constructor
+	 *
+	 * @param AbstractList $List List instance
+	 * @return ListIterator
+	 */
 	function ListIterator($List) {
 		parent::PHP2Go();
-		if (!TypeUtils::isObject($List) || !TypeUtils::isInstanceOf($List, 'AbstractList')) {
+		if (!TypeUtils::isInstanceOf($List, 'AbstractList')) {
 			PHP2Go::raiseError(PHP2Go::getLangVal('ERR_INVALID_OBJECT', 'AbstractList'), E_USER_ERROR, __FILE__, __LINE__);
 		} else {
 			$this->current = -1;
@@ -55,23 +74,21 @@ class ListIterator extends PHP2Go
 		}
 	}
 
-	//!-----------------------------------------------------------------
-	// @function	ListIterator::getCurrentIndex
-	// @desc		Busca a posição atual do cursor na lista de elementos
-	// @access		public
-	// @return		int Posição atual do cursor
-	//!-----------------------------------------------------------------
+	/**
+	 * Gets current cursor position
+	 *
+	 * @return int
+	 */
 	function getCurrentIndex() {
 		return $this->current;
 	}
 
-	//!-----------------------------------------------------------------
-	// @function	ListIterator::moveToIndex
-	// @desc		Muda a posição atual do cursor na lista de elementos
-	// @access		public
-	// @param		index int 	Nova posição para o cursor
-	// @return		bool
-	//!-----------------------------------------------------------------
+	/**
+	 * Moves to a given list index
+	 *
+	 * @param int $index Target index
+	 * @return bool
+	 */
 	function moveToIndex($index) {
 		if ($index >= 0 && $index < $this->_List->size()) {
 			$this->current = $index - 1;
@@ -80,32 +97,31 @@ class ListIterator extends PHP2Go
 		return FALSE;
 	}
 
-	//!-----------------------------------------------------------------
-	// @function	ListIterator::current
-	// @desc		Retorna o elemento atual da lista
-	// @access		public
-	// @return		mixed Valor do elemento
-	//!-----------------------------------------------------------------
+	/**
+	 * Gets current list element
+	 *
+	 * @return mixed
+	 */
 	function current() {
 		return ($this->getCurrentIndex() >= 0) ? $this->_List->get($this->current + 1) : NULL;
 	}
 
-	//!-----------------------------------------------------------------
-	// @function	ListIterator::hasNext
-	// @desc		Verifica se a lista possui mais elementos quando percorrida na ordem crescente
-	// @access		public
-	// @return		bool
-	//!-----------------------------------------------------------------
+	/**
+	 * Check if there's a next item to be read
+	 *
+	 * @return bool
+	 */
 	function hasNext() {
 		return ($this->current < ($this->_List->size() - 1));
 	}
 
-	//!-----------------------------------------------------------------
-	// @function	ListIterator::next
-	// @desc		Busca o próximo elemento da lista
-	// @access		public
-	// @return		mixed Próximo elemento ou FALSE se a lista atingiu o final
-	//!-----------------------------------------------------------------
+	/**
+	 * Moves cursor to the next position and returns the fetched element.
+	 *
+	 * Returns FALSE when the cursor is already at the last position.
+	 *
+	 * @return mixed
+	 */
 	function next() {
 		if ($this->hasNext()) {
 			return $this->_List->get(++$this->current);
@@ -114,32 +130,31 @@ class ListIterator extends PHP2Go
 		}
 	}
 
-	//!-----------------------------------------------------------------
-	// @function	ListIterator::nextIndex
-	// @desc		Busca o índice retornado pela próxima execução do método next()
-	// @access		public
-	// @return		int Valor do índice
-	//!-----------------------------------------------------------------
+	/**
+	 * Gets next list index
+	 *
+	 * @return int
+	 */
 	function nextIndex() {
 		return $this->current + 1;
 	}
 
-	//!-----------------------------------------------------------------
-	// @function	ListIterator::hasPrevious
-	// @desc		Verifica se a lista possui mais elementos quando percorrida na ordem decrescente
-	// @access		public
-	// @return		bool
-	//!-----------------------------------------------------------------
+	/**
+	 * Checks if there's a previous item before the current one
+	 *
+	 * @return bool
+	 */
 	function hasPrevious() {
 		return ($this->current > 0);
 	}
 
-	//!-----------------------------------------------------------------
-	// @function	ListIterator::previous
-	// @desc		Busca o elemento anterior na lista
-	// @access		public
-	// @return		mixed Elemento anterior ou FALSE se a lista atingiu o início
-	//!-----------------------------------------------------------------
+	/**
+	 * Moves the cursor to the previous index and return the fetched element
+	 *
+	 * Returns FALSE when the cursor is already at the first position.
+	 *
+	 * @return unknown
+	 */
 	function previous() {
 		if ($this->hasPrevious()) {
 			return $this->_List->get(--$this->current);
@@ -148,12 +163,11 @@ class ListIterator extends PHP2Go
 		}
 	}
 
-	//!-----------------------------------------------------------------
-	// @function	ListIterator::previousIndex
-	// @desc		Busca o índice retornado pela próxima execução do método previous()
-	// @access		public
-	// @return		int Valor do índice
-	//!-----------------------------------------------------------------
+	/**
+	 * Gets the previous list index
+	 *
+	 * @return int
+	 */
 	function previousIndex() {
 		return $this->current - 1;
 	}
