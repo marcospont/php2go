021f429946acbc0e965de23dffcd40818ab1f292
marcos.pont <marcos.pont@713797e6-a6b2-11dd-9cdb-aff5a21a0eb7>
2007-03-23 07:08:30 +0000

    Added slide show toolbar (play/pause button, current/total counter, image description and set delay input) Support was added for loading slide show images from a given folder

--- core/gui/widget/SlideShow.class.php
+++ core/gui/widget/SlideShow.class.php
@@ -36,8 +36,14 @@
  * Available attributes
  * # id : widget ID
  * # images : array of images of the slide show (mandatory)
+ * # imagesFolder : a folder name where the images should be collected from
+ * # imagesFolderFilter : the filter that must be applied when listing the images folder
  * # delay : slide show delay, in microseconds
+ * # play : initial state (playing or stopped)
+ * # playCaption : button caption when stopped
+ * # pauseCaption : button caption when playing
  * # class : CSS class for the slide show container
+ * # toolbarClass : CSS class for the slide show toolbar
  * # width : width for the slide show container
  * # height : height for the slide show container
  *
@@ -79,8 +85,15 @@ class SlideShow extends Widget
 	function getDefaultAttributes() {
 		return array(
 			'id' => PHP2Go::generateUniqueId(parent::getClassName()),
+			'images' => array(),
+			'imagesFolder' => '',
+			'imagesFolderFilter' => '',
 			'delay' => 4000,
+			'play' => TRUE,
+			'playCaption' => 'Play',
+			'pauseCaption' => 'Pause',
 			'class' => '',
+			'toolbarClass' => '',
 			'width' => '400px',
 			'height' => '400px'
 		);
@@ -93,10 +106,21 @@ class SlideShow extends Widget
 	 * @param array $attrs Attributes
 	 */
 	function loadAttributes($attrs) {
+		// normalize dimensions
 		if (is_int($attrs['width']))
 			$attrs['width'] .= 'px';
 		if (is_int($attrs['height']))
 			$attrs['height'] .= 'px';
+		// load images from a folder
+		if (empty($attrs['images']) && !empty($attrs['imagesFolder'])) {
+			import('php2go.file.DirectoryManager');
+			$Dir = new DirectoryManager();
+			$Dir->open($attrs['imagesFolder']);
+			while ($Entry = $Dir->read(strval($attrs['imagesFolderFilter']))) {
+				$attrs['images'][] = array('url' => rtrim($attrs['imagesFolder'], '/') . '/' . $Entry->getName(), 'description' => $Entry->getName());
+			}
+			$Dir->close();
+		}
 		parent::loadAttributes($attrs);
 	}
 
@@ -114,14 +138,25 @@ class SlideShow extends Widget
 	 */
 	function render() {
 		$attrs =& $this->attributes;
-		$code = sprintf("\n<div id=\"%s\" style=\"position:relative;width:%s;height:%s;\"%s>", $attrs['id'], $attrs['width'], $attrs['height'], (!empty($attrs['class']) ? " class=\"{$attrs['class']}\"" : ""));
-		$code .= sprintf("\n  <img id=\"%s_foreground\" style=\"z-index:1;width:%s;height:%s;\" class=\"slideShowImg\">", $attrs['id'], $attrs['width'], $attrs['height']);
-		$code .= sprintf("\n  <img id=\"%s_background\" style=\"z-index:0;width:%s;height:%s;\" class=\"slideShowImg\">", $attrs['id'], $attrs['width'], $attrs['height']);
+		$code = sprintf("\n<div id=\"%s\"%s style=\"width:%s;\">", $attrs['id'], (!empty($attrs['class']) ? " class=\"{$attrs['class']}\"" : ""), $attrs['width'], $attrs['height']);
+		$code .= sprintf("\n  <div style=\"position:relative;width:%s;height:%s;vertical-align:middle\">", $attrs['width'], $attrs['height']);
+		$code .= sprintf("\n    <img id=\"%s_foreground\" style=\"z-index:1;visibility:hidden;\" class=\"slideShowImg\">", $attrs['id']);
+		$code .= sprintf("\n    <img id=\"%s_background\" style=\"z-index:0;visibility:hidden;\" class=\"slideShowImg\">", $attrs['id']);
+		$code .= "\n  </div>";
+		$code .= sprintf("\n  <table id=\"%s_toolbar\" width=\"100%%\" cellpadding=\"3\" cellspacing=\"0\" border=\"0\" style=\"margin-top:5px;\"%s><tr>", $attrs['id'], (!empty($attrs['toolbarClass']) ? " class=\"{$attrs['toolbarClass']}\"" : ""));
+		$code .= sprintf("\n    <td align=\"left\"><button id=\"%s_toggle\" type=\"button\">%s</button><span id=\"%s_count\" style=\"margin-left:8px;\"></span></td>", $attrs['id'], ($attrs['play'] ? $attrs['pauseCaption'] : $attrs['playCaption']), $attrs['id']);
+		$code .= sprintf("\n    <td align=\"center\" id=\"%s_description\"></td>", $attrs['id']);
+		$code .= sprintf("\n    <td align=\"right\">%s<input id=\"%s_delay\" type=\"text\" size=\"4\" maxlength=\"2\" value=\"%s\" class=\"slideShowInput\"></td>", 'Delay', $attrs['id'], ($attrs['delay']/1000));
+		$code .= "\n  </tr></table>";
 		$code .= "\n</div>";
 		$code .= sprintf("\n<script type=\"text/javascript\">Widget.init(\"%s\", %s);</script>", 'SlideShow', JSONEncoder::encode(array(
 			'id' => $attrs['id'],
+			'width' => intval(preg_replace("/[^0-9]+/", "", $this->attributes['width'])),
 			'images' => $attrs['images'],
-			'delay' => $attrs['delay']
+			'delay' => $attrs['delay'],
+			'play' => $attrs['play'],
+			'playCaption' => $attrs['playCaption'],
+			'pauseCaption' => $attrs['pauseCaption']
 		)));
 		print $code;
 	}
