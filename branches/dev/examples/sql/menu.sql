3115465da4b9d2486c26b0f281b4d071f2abea16
marcos.pont <marcos.pont@713797e6-a6b2-11dd-9cdb-aff5a21a0eb7>
2007-04-27 01:53:56 +0000

    Small fixes on SQL files used by the examples

--- branches/dev/examples/sql/menu.sql
+++ branches/dev/examples/sql/menu.sql
@@ -1,21 +1,20 @@
-DROP TABLE IF EXISTS `menu`;
-CREATE TABLE IF NOT EXISTS `menu` (
-  `id_menu` int(11) unsigned NOT NULL auto_increment,
-  `id_parent_menu` int(11) unsigned default NULL,
-  `caption` varchar(50) NOT NULL default '',
-  `link` varchar(100) default NULL,
-  PRIMARY KEY  (`id_menu`)
-) TYPE=MyISAM COMMENT='Menu';
-TRUNCATE TABLE `menu`;
-INSERT INTO `menu` (id_parent_menu, caption, link) VALUES (null, 'Products', 'products.php');
-INSERT INTO `menu` (id_parent_menu, caption, link) VALUES (1, 'Books »', null);
-INSERT INTO `menu` (id_parent_menu, caption, link) VALUES (2, 'Fiction', 'products.php?category=1');
-INSERT INTO `menu` (id_parent_menu, caption, link) VALUES (2, 'Non-Fiction', 'products.php?category=2');
-INSERT INTO `menu` (id_parent_menu, caption, link) VALUES (1, 'Toys', 'products.php?category=3');
-INSERT INTO `menu` (id_parent_menu, caption, link) VALUES (1, 'Audio & Video', 'products.php?category=4');
-INSERT INTO `menu` (id_parent_menu, caption, link) VALUES (null, 'Online Store', null);
-INSERT INTO `menu` (id_parent_menu, caption, link) VALUES (7, 'About us', 'about.php');
-INSERT INTO `menu` (id_parent_menu, caption, link) VALUES (7, 'Order information', 'order_info.php');
-INSERT INTO `menu` (id_parent_menu, caption, link) VALUES (7, 'Online support', 'online_support.php');
-INSERT INTO `menu` (id_parent_menu, caption, link) VALUES (null, 'My Cart', 'cart.php');
-INSERT INTO `menu` (id_parent_menu, caption, link) VALUES (null, 'Logout', 'logout.php');
+DROP TABLE IF EXISTS menu;
+CREATE TABLE menu (
+  id_menu int(11) unsigned NOT NULL auto_increment,
+  id_parent_menu int(11) unsigned default NULL,
+  caption varchar(50) NOT NULL default '',
+  link varchar(100) default NULL,
+  PRIMARY KEY  (id_menu)
+) AUTO_INCREMENT=13 COMMENT='Menu';
+insert into menu (id_menu, id_parent_menu, caption, link) values (1, NULL, 'Products', '');
+insert into menu (id_menu, id_parent_menu, caption, link) values (2, 1, 'Books »', NULL);
+insert into menu (id_menu, id_parent_menu, caption, link) values (3, 2, 'Fiction', '?op=Books/Fiction');
+insert into menu (id_menu, id_parent_menu, caption, link) values (4, 2, 'Non-Fiction', '?op=Books/Non-Fiction');
+insert into menu (id_menu, id_parent_menu, caption, link) values (5, 1, 'Toys', '?op=Toys');
+insert into menu (id_menu, id_parent_menu, caption, link) values (6, 1, 'Audio & Video', '?op=Audio/Video');
+insert into menu (id_menu, id_parent_menu, caption, link) values (7, NULL, 'Online Store »', NULL);
+insert into menu (id_menu, id_parent_menu, caption, link) values (8, 7, 'About us', '?op=About');
+insert into menu (id_menu, id_parent_menu, caption, link) values (9, 7, 'Order information', '?op=Order Info');
+insert into menu (id_menu, id_parent_menu, caption, link) values (10, 7, 'Online support', '?op=Support');
+insert into menu (id_menu, id_parent_menu, caption, link) values (11, NULL, 'My Cart', '?op=Cart');
+insert into menu (id_menu, id_parent_menu, caption, link) values (12, NULL, 'Logout', '?op=Logout');
