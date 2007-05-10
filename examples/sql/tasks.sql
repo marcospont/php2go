d86d0f3c00945b5ad2aacb6b193e5818bc800413
marcos.pont <marcos.pont@713797e6-a6b2-11dd-9cdb-aff5a21a0eb7>
2007-05-10 21:23:55 +0000

    Added missing column in tasks table creation script

--- examples/sql/tasks.sql
+++ examples/sql/tasks.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS tasks;
 CREATE TABLE tasks (
   id_task int(11) unsigned NOT NULL auto_increment,
+  id_project int(11) NOT NULL default '0',
   id_owner int(11) NOT NULL default '0',
   name varchar(20) NOT NULL default '',
   description varchar(200) NOT NULL default '',
