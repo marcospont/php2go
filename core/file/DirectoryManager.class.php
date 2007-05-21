1f1559da9a0d40e4e8f157d42855e6aa975ac0dd
marcos.pont <marcos.pont@713797e6-a6b2-11dd-9cdb-aff5a21a0eb7>
2007-05-21 04:34:55 +0000

    Replace ereg by preg_match

--- core/file/DirectoryManager.class.php
+++ core/file/DirectoryManager.class.php
@@ -406,7 +406,7 @@ class DirectoryManager extends PHP2Go
 		if (is_resource($this->currentHandle)) {
 			if ($entry = @readdir($this->currentHandle)) {
 				// ignore "." and ".." entries
-				if (ereg("^\.{1,2}", $entry)) {
+				if (preg_match("/^\.{1,2}/", $entry)) {
 					return $this->_readSimple($includeRegExp);
 				} elseif (!empty($includeRegExp)) {
 					if (preg_match('/' . $includeRegExp . '/', $entry))
