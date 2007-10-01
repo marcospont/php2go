fcc3f085e97c6dfa60af722a161ecf7c9d2e4e94
marcos.pont <marcos.pont@713797e6-a6b2-11dd-9cdb-aff5a21a0eb7>
2007-10-01 05:17:58 +0000

    Bug fix on Element.prototype.setOpacity Changed Observable integration with other classes Added Object.isIterable Changed Collection.invoke to use walk instead of map

--- branches/dev/resources/javascript/structures.js
+++ branches/dev/resources/javascript/structures.js
@@ -202,7 +202,7 @@ var Collection = {
 	 */
 	invoke : function(method) {
 		var args = Array.prototype.slice.call(arguments, 1);
-		return this.map(function(item, idx) {
+		this.walk(function(item, idx) {
 			(item[method]) && (item[method].apply(item, args));
 		});
 	},
