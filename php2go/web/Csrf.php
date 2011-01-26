36073e4dbf43fdef871e364735166b8c51f69b8d
marcos.pont <marcos.pont@713797e6-a6b2-11dd-9cdb-aff5a21a0eb7>
2011-01-26 23:31:38 +0000

    Allow exceptions on CSRF validation (an array of routes where the validation needs to be skipped)

--- php2go/web/Csrf.php
+++ php2go/web/Csrf.php
@@ -17,12 +17,24 @@ abstract class Csrf
 
 	public static function validate() {
 		$request = Php2Go::app()->getRequest();
-		if ($request->isPost()) {
+		if ($request->isPost() && !self::isException()) {
 			$current = self::getToken();
 			$provided = $request->getPost(self::TOKEN_NAME);
 			if ($provided === null || $provided !== $current)
 				throw new HttpException(403, __(PHP2GO_LANG_DOMAIN, 'The CSRF token could not be verified.'));
 		}
+	}
+
+	private static function isException() {
+		$app = Php2Go::app();
+		$validation = $app->getCsrfValidation();
+		if (is_array($validation['exceptions'])) {
+			foreach ($validation['exceptions'] as $exception) {
+				if (strpos($app->getRequest()->getPathInfo(), $exception) === 0)
+					return true;
+			}
+		}
+		return false;
 	}
 
 	private static function generateToken() {
