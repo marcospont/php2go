ad680525b202f93cd004124d4ec67cc578f64a91
marcos.pont <marcos.pont@713797e6-a6b2-11dd-9cdb-aff5a21a0eb7>
2011-03-29 02:15:41 +0000

    Flash: added $default parameter to get() Util: added setArrayPath() Module: setOption() now accepts an array path (dot separated)

--- branches/reboot/php2go/web/Flash.php
+++ branches/reboot/php2go/web/Flash.php
@@ -3,17 +3,17 @@
 final class Flash
 {
 	const SESSION_VAR = '__flash__';
-	
-	public static function get($key) {
+
+	public static function get($key, $default=null) {
 		$session = Php2Go::app()->getSession();
 		$value = $session->get($key);
 		if ($value) {
 			$session->remove($key);
 			return $value;
 		}
-		return null;
+		return $default;
 	}
-	
+
 	public static function set($key, $value) {
 		$session = Php2Go::app()->getSession();
 		$session->set($key, $value);
